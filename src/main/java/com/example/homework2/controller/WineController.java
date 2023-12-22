78dd76a423801b287ab510eb73f09c66933b7349
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-12-22 19:09:05 +0900

    homework 20231216

--- src/main/java/com/example/homework2/controller/WineController.java
+++ src/main/java/com/example/homework2/controller/WineController.java
@@ -1,5 +1,6 @@
 package com.example.homework2.controller;
 
+import com.example.homework2.model.Wine;
 import com.example.homework2.util.LoadCsvUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,19 +20,31 @@ public class WineController {
     List<String[]> splittedCsvList = LoadCsvUtil.splitStringList(rawCsvList);
 
 
-    // todo: splittedCsvListをWineクラスのリストに変換する
-//    List</*定義したWineクラス*/> wineList = /* Wineクラスのリスト */;
+    int WINE_INDEX_COUNTRY = 1;
+    int WINE_INDEX_POINTS = 4;
+    int WINE_INDEX_PRICE = 5;
+    int WINE_INDEX_PROVINCE = 6;
 
+    List<Wine> wineList = splittedCsvList.stream()
+            .map(l -> new Wine(
+                    l[WINE_INDEX_COUNTRY],
+                    l[WINE_INDEX_POINTS],
+                    l[WINE_INDEX_PRICE],
+                    l[WINE_INDEX_PROVINCE])
+            )
+            .toList();
 
+    /** wine-1k.csvのを文字列リストにしただけのリストを返す */
     @GetMapping("getRawCsv")
     public List<String> getRawCsv(){
         return rawCsvList;
     }
 
-
-    // todo: wineListを返すエンドポイント作成する
-//    @GetMapping("getWineList")
-//    public List</*定義したWineクラス*/> getWineList(){
+    /** wineクラスのリストを返す */
+    @GetMapping("getWineList")
+    public List<Wine> getWineList(){
+        return wineList;
+    }
 
 
 }
